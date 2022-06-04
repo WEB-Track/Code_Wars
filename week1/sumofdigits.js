3c36513f0f5665b610f6588970eebd6ecd35403e
Bill B <web-dog@live.com>
2022-06-03 22:31:06 -0400

    adding 1 codewars

---
+++ week1/sumofdigits.js
@@ -0,0 +1,8 @@
+// get the sum of all the digits in n
+// if the sum is more than one digit get the new sum of the new digits. until the sum is a one digit number
+// the input will be non negative number
+
+function digital_root(n) {
+  if (n > 9) {
+  }
+}
